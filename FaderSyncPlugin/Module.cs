0129f649d3761ed265a48bc0475e6ddb2dd0932c
Parzival <me@parzival.space>
2024-04-21 02:57:06 +0200

    Use InformationalVersion instead of AssemblyFileVersion

--- FaderSyncPlugin/Module.cs
+++ FaderSyncPlugin/Module.cs
@@ -5,5 +5,8 @@ namespace FaderSync;
 public static class Module
 {
     public static readonly string Name = Assembly.GetExecutingAssembly()?.GetName()?.Name!;
-    public static readonly string Version = Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString()!;
+
+    public static readonly string Version = Assembly.GetExecutingAssembly()?
+        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion!
+        .Split('+')[0]!; // remove commit hash
 }
