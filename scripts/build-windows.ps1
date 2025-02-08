9092badf027c32ab2e54ec66861350a477b197c1
Malte Linke (parzival-space) <me@parzival.space>
2025-02-08 02:58:21 +0100

    Rename build script and update paths for Linux and Windows configurations

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,5 +1,6 @@
 $obsPath = "$env:ProgramFiles\obs-studio"
 $obsFile = "$obsPath\bin\64bit\obs64.exe"
+$installPath = "C:\ProgramData\obs-studio\plugins\FaderSyncPlugin"
 
 # Check if OBS is running
 $obsProcess = Get-Process | Where-Object { $_.MainModule.FileName -like "$obsFile" }
@@ -20,7 +21,13 @@ if ($LastExitCode -ne 0) {
 }
 
 # Copy plugin to OBS plugin path
-Copy-Item -Path ".\FaderSyncPlugin\bin\Release\net8.0\win-x64\publish\FaderSyncPlugin.dll" -Destination "$obsPath\obs-plugins\64bit\obs-goxlr-fader-sync.dll" -Force
+if (Test-Path $installPath) {
+    Write-Host "Removing old plugin files"
+    Remove-Item -Path "$installPath" -Recurse -Force
+}
+Write-Host "Copying new plugin files"
+Copy-Item -Recurse -Path "./FaderSyncPlugin/bin/Release/net8.0/win-x64/package/package-src/FaderSyncPlugin" -Destination "$installPath" -Force
 
 # Start OBS
+Write-Host "Starting OBS"
 Start-Process "$obsFile" -WorkingDirectory "$obsPath\bin\64bit"
